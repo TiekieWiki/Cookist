e146394e1a4aa1236fed86f8b509ee572529b393
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2025-04-16 19:51:19 +0200

    Edit cook group

--- src/components/AddCookGroup.vue
+++ src/components/AddCookGroup.vue
@@ -34,7 +34,7 @@
       </input-list>
       <error-message v-model:message="errorMessage" />
       <div class="footer">
-        <button @click="$emit('closePopUp')" type="button">
+        <button @click="closePopUp" type="button">
           {{ $t('addCookGroupPage.cancel') }}
         </button>
         <button @click.prevent="saveCookGroup" type="submit">
@@ -46,35 +46,51 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { capitalize, onMounted, ref } from 'vue';
 import InputField from './form/InputField.vue';
 import InputList from './form/InputList.vue';
 import ErrorMessage from './form/ErrorMessage.vue';
 import { addInputRow } from '@/utils/newRecipe/list';
 import { validateCookGroup } from '@/utils/cookGroups/validateCookGroup';
 import { emptyCookGroupMembers, type CookGroup, type Member } from '@/utils/types/cookgroup';
-import { addData } from '@/utils/db';
+import { addData, updateData } from '@/utils/db';
 import i18n from '@/i18n/index';
 import { getAuth } from 'firebase/auth';
+import { where } from 'firebase/firestore';
+
+const props = defineProps<{
+  cookGroup?: CookGroup;
+}>();
+const emit = defineEmits<{ closePopUp: []; refreshCookGroups: [] }>();
 
 const auth = getAuth();
-
-const emit = defineEmits<{ closePopUp: []; refreshCookGroups: [] }>();
 
 const cookGroup = ref<CookGroup>(emptyCookGroupMembers());
 const errorMessage = ref<string>('');
 
+// Add cook group to edit if it exists
+onMounted(() => {
+  if (props.cookGroup) {
+    cookGroup.value = JSON.parse(JSON.stringify(props.cookGroup));
+    cookGroup.value.name = capitalize(cookGroup.value.name);
+    cookGroup.value.members = [...cookGroup.value.members, { userId: '', email: '' }];
+  }
+});
+
 /**
  * Save the new cook group
+ * @returns {Promise<void>} - A promise that resolves when the cook group is saved
  */
-async function saveCookGroup() {
+async function saveCookGroup(): Promise<void> {
   errorMessage.value = validateCookGroup(cookGroup.value);
   const members = cookGroup.value.members.filter((member) => member.email !== '');
   if (errorMessage.value === '') {
-    cookGroup.value.id = crypto.randomUUID();
+    if (!props.cookGroup) {
+      cookGroup.value.id = crypto.randomUUID();
+      cookGroup.value.owner = auth.currentUser?.uid || '';
+      cookGroup.value.personal = false;
+    }
     cookGroup.value.name = cookGroup.value.name.toLowerCase();
-    cookGroup.value.owner = auth.currentUser?.uid || '';
-    cookGroup.value.personal = false;
     cookGroup.value.members = [
       ...new Set(
         members.map((member) => ({
@@ -86,7 +102,12 @@ async function saveCookGroup() {
 
     // Save the cook group to the database
     try {
-      await addData('cookGroups', cookGroup.value);
+      // Check if the user is editing an existing cook group
+      if (props.cookGroup) {
+        await updateData('cookGroups', where('id', '==', cookGroup.value.id), cookGroup.value);
+      } else {
+        await addData('cookGroups', cookGroup.value);
+      }
 
       // Reset the form
       cookGroup.value = emptyCookGroupMembers();
@@ -99,5 +120,14 @@ async function saveCookGroup() {
       errorMessage.value = i18n.global.t('addCookGroupPage.errors.save');
     }
   }
+}
+
+/**
+ * Close the pop-up and reset the form
+ */
+function closePopUp(): void {
+  emit('closePopUp');
+  cookGroup.value = emptyCookGroupMembers();
+  errorMessage.value = '';
 }
 </script>
--- src/views/CookGroupsView.vue
+++ src/views/CookGroupsView.vue
@@ -20,6 +20,12 @@
       <article v-for="cookGroup in cookGroups" :key="cookGroup.id" class="card" :id="cookGroup.id">
         <div class="title">
           <h3>{{ capitalizeFirstLetter(cookGroup.name) }}</h3>
+          <font-awesome-icon
+            v-if="cookGroup.owner === getAuth().currentUser?.uid"
+            @click="editCookGroup(cookGroup)"
+            class="editCookGroup"
+            :icon="['fas', 'pen']"
+          />
         </div>
         <div class="info">
           <p>{{ 1 }} {{ true ? $t('cookGroupsPage.recipes') : $t('cookGroupsPage.recipe') }}</p>
@@ -42,7 +48,8 @@
   </main>
   <AddCookGroup
     v-if="addCookGroupOpen"
-    @close-pop-up="addCookGroupOpen = !addCookGroupOpen"
+    :cook-group="editableCookGroup"
+    @close-pop-up="closePopUp()"
     @refresh-cook-groups="getCookGroups()"
   />
 </template>
@@ -54,10 +61,12 @@ import type { CookGroup } from '@/utils/types/cookgroup';
 import { getData } from '@/utils/db';
 import { getQueryCookGroups } from '@/utils/cookGroups/queryCookGroups';
 import { capitalizeFirstLetter } from '@/utils/text';
+import { getAuth } from 'firebase/auth';
 
 const cookGroups = ref<CookGroup[]>([]);
 const noCookGroups = ref<boolean>(true);
 const addCookGroupOpen = ref<boolean>(false);
+const editableCookGroup = ref<CookGroup | undefined>(undefined);
 
 // Get cook groups
 onMounted(async () => {
@@ -75,5 +84,23 @@ async function getCookGroups(): Promise<void> {
   } catch (error) {
     noCookGroups.value = true;
   }
+}
+
+/**
+ * Edit cook group
+ * @returns {void}
+ */
+function editCookGroup(cookGroup: CookGroup): void {
+  addCookGroupOpen.value = true;
+  editableCookGroup.value = cookGroup;
+}
+
+/**
+ * Close the pop up
+ * @returns {void}
+ */
+function closePopUp(): void {
+  addCookGroupOpen.value = false;
+  editableCookGroup.value = undefined;
 }
 </script>
