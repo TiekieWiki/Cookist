c70d0460f867bf697070cc2436e880674c7d4ee7
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2025-08-12 17:06:49 +0200

    Show amount of recipes in cook group

--- src/components/cook group/CookGroupCard.vue
+++ src/components/cook group/CookGroupCard.vue
@@ -15,7 +15,10 @@
     />
   </section>
   <section class="info">
-    <p>{{ 1 }} {{ true ? $t('cookGroupsPage.recipes') : $t('cookGroupsPage.recipe') }}</p>
+    <p>
+      {{ cookGroupRecipesCount }}
+      {{ cookGroupRecipesCount == 1 ? $t('cookGroupsPage.recipe') : $t('cookGroupsPage.recipes') }}
+    </p>
     <p>
       {{ cookGroup.members.length }}
       {{
@@ -29,14 +32,30 @@
 import type { CookGroup } from '@/utils/types/cookgroup';
 import { capitalizeFirstLetter } from '@/utils/text';
 import { getAuth } from 'firebase/auth';
+import { onMounted, ref } from 'vue';
+import { getData } from '@/utils/db';
+import { where } from 'firebase/firestore';
 
-defineProps<{
+const props = defineProps<{
   cookGroup: CookGroup;
 }>();
 
 const editCookGroupOpen = defineModel<boolean>('editCookGroupOpen', { required: true });
 const editableCookGroup = defineModel<CookGroup | undefined>('editableCookGroup', {
   required: true
+});
+
+const cookGroupRecipesCount = ref<number>();
+
+onMounted(() => {
+  // Get cook group recipes if they exist
+  getData('cookGroupRecipes', where('cookGroupId', '==', props.cookGroup.id))
+    .then((data) => {
+      cookGroupRecipesCount.value = data.length;
+    })
+    .catch((error) => {
+      console.error('Error getting cook group recipes from database:', error);
+    });
 });
 
 /**
