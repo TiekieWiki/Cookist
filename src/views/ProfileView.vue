afed0a2179dfb3a549dafa21dd4c94c13c6a67e9
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2025-08-14 20:52:49 +0200

    Solved delete account not complete

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -137,16 +137,45 @@ const deleteAccountOpen = ref<boolean>(false);
 async function deleteAccount(): Promise<void> {
   // Delete the user from the database
   deleteData('users', where('id', '==', auth.currentUser?.uid))
-    .then(() => {
-      // Delete the user
-      deleteUser(auth.currentUser as any);
+    .catch((err) => {
+      console.error('Error deleting user record:', err);
+    })
+    .then(async () => {
+      // Delete the user's recipes
+      return deleteData('recipes', where('owner', '==', auth.currentUser?.uid)).catch((error) => {
+        console.error('Error deleting recipes:', error);
+      });
+    })
+    .then(async () => {
+      // Get the user's cook groups
+      return getData('cookGroups', where('owner', '==', auth.currentUser?.uid)).catch((err) => {
+        console.error('Error getting cook groups:', err);
+        return [];
+      });
+    })
+    .then((cookGroups) => {
+      // Delete the user's cook group recipes
+      cookGroups.forEach((cookGroup) => {
+        deleteData('cookGroupRecipes', where('cookGroupId', '==', cookGroup.id)).catch((err) => {
+          console.error(`Error deleting recipes for cook group ${cookGroup.id}:`, err);
+        });
+      });
+    })
+    .then(async () => {
+      // Delete the user's cook groups
+      return deleteData('cookGroups', where('owner', '==', auth.currentUser?.uid)).catch((err) => {
+        console.error('Error deleting cook groups:', err);
+      });
+    })
+    .then(async () => {
+      // Delete the user auth account
+      return deleteUser(auth.currentUser as any).catch((err) => {
+        console.error('Error deleting auth user:', err);
+      });
     })
     .then(() => {
       // Redirect to recipes page
       router.push({ path: '/' });
-    })
-    .catch((error) => {
-      console.error('Error deleting account:', error);
     });
 }
 </script>
