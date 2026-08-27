d02f48cb908535336a9d885f1618af7b2f295c37
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2026-08-27 22:26:22 +0200

    Confirm popup

--- src/components/general/ConfirmPopUp.vue
+++ src/components/general/ConfirmPopUp.vue
@@ -1,39 +1,35 @@
 <template>
   <teleport to="body" v-if="openPopUp">
     <div class="overlay"></div>
-    <main>
-      <article class="card">
-        <section class="header">
-          <h2>{{ $t(title) }}</h2>
-          <Button
-            @click="openPopUp = false"
-            :type="ButtonType.BUTTON"
-            :variant="ColorVariant.SECONDARY"
-          >
-            <font-awesome-icon :icon="['fas', 'xmark']" />
-          </Button>
-        </section>
-        <section>
-          <p>{{ $t(section) }}</p>
-        </section>
-        <section class="footer">
-          <Button
-            @click="openPopUp = false"
-            :type="ButtonType.BUTTON"
-            :variant="ColorVariant.SECONDARY"
-          >
-            {{ $t(cancel) }}
-          </Button>
-          <Button
-            @click.prevent="emit('confirm', true)"
-            :type="ButtonType.SUBMIT"
-            :variant="ColorVariant.WARNING"
-          >
-            {{ $t(confirm) }}
-          </Button>
-        </section>
-      </article>
-    </main>
+    <article class="confirmPopUp">
+      <div class="title">
+        <h2>{{ $t(title) }}</h2>
+        <Button
+          @click="openPopUp = false"
+          :type="ButtonType.BUTTON"
+          :variant="ColorVariant.TERTIARY"
+        >
+          <font-awesome-icon :icon="['fas', 'xmark']" />
+        </Button>
+      </div>
+      <p>{{ $t(section) }}</p>
+      <div class="footer">
+        <Button
+          @click="openPopUp = false"
+          :type="ButtonType.BUTTON"
+          :variant="ColorVariant.SECONDARY"
+        >
+          {{ $t(cancel) }}
+        </Button>
+        <Button
+          @click.prevent="emit('confirm', true)"
+          :type="ButtonType.SUBMIT"
+          :variant="ColorVariant.WARNING"
+        >
+          {{ $t(confirm) }}
+        </Button>
+      </div>
+    </article>
   </teleport>
 </template>
 
